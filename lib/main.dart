9c541b9d2dc959c7d0cf22dba3f65f90173944cd
Naman <vyasn30@outlook.com>
2020-01-09 14:58:35 +0530

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:veyg_2020/pages/department_page.dart';
 
-void main(){
-  runApp(MaterialApp(title: "Veyg 2020",home:MyApp() ,));
+void main() {
+  runApp(MaterialApp(
+    title: "Veyg 2020",
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -13,6 +18,17 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Timer(Duration(seconds: 4), () {
+      Navigator.push(context, MaterialPageRoute(builder: (context){
+        return DepartmentPage();
+      }));
+    });
+    return Scaffold(
+
+      body: Center(
+        child: Image.asset("assets/logo.png"),
+
+      ),
+    );
   }
 }
