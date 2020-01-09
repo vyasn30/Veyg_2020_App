fd6b264c28b4b23bc3199e987b5b9e7f1e58ae60
Naman <vyasn30@outlook.com>
2020-01-09 13:54:23 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(title: "Veyg 2020",home:MyApp() ,));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
