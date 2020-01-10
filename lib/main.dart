66b510b00ef27906c4a8ccc3d4c9d3f122461c11
Hiren <hirenvadher954@gmail.com>
2020-01-10 12:58:50 +0530

    v 1.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:veyg_2020/pages/events_page.dart';
 import 'package:veyg_2020/pages/splash_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Veyg2020",
-    home: SplashScreen(),
+    home: EventsPage(),
   ));
 }
 
