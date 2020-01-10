ecf6b99ff17cb94440d6e00804cda026f819c52e
Hiren <hirenvadher954@gmail.com>
2020-01-10 19:19:29 +0530

    v 1.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:veyg_2020/pages/events_page.dart';
-import 'package:veyg_2020/pages/splash_screen.dart';
+
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: "Veyg2020",
     home: EventsPage(),
   ));
--- lib/pages/events_page.dart
+++ lib/pages/events_page.dart
@@ -1,137 +1,129 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:folding_cell/folding_cell/widget.dart';
-import 'package:gradient_widgets/gradient_widgets.dart';
 
-import 'event_detail.dart';
+List<String> temp = ["Hiren","Vadher","Raju"];
+List<String> value = ["A","B","C"];
 
 class EventsPage extends StatelessWidget {
-  final _foldingCellKey1 = GlobalKey<SimpleFoldingCellState>();
-  final _foldingCellKey2 = GlobalKey<SimpleFoldingCellState>();
-  final _foldingCellKey3 = GlobalKey<SimpleFoldingCellState>();
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Text(
-        "Demo",
-      )),
-      body: Container(
-        color: Color(0xFF2e282a),
-        child: ListView(
-          children: <Widget>[
-            SimpleFoldingCell(
-                key: _foldingCellKey1,
-                frontWidget: _buildFrontWidget(_foldingCellKey1, "CARD 1"),
-                innerTopWidget: _buildInnerTopWidget(_foldingCellKey1,"TITLE 1", context),
-                innerBottomWidget: _buildInnerBottomWidget(_foldingCellKey1,context),
+    return Container(
+      color: Color(0xFF2e282a),
+      child: ListView.builder(
+          itemCount: temp.length,
+          itemBuilder: (context, index) {
+            return SimpleFoldingCell(
+                frontWidget: _buildFrontWidget(index),
+                innerTopWidget: _buildInnerTopWidget(index),
+                innerBottomWidget: _buildInnerBottomWidget(index),
                 cellSize: Size(MediaQuery.of(context).size.width, 125),
                 padding: EdgeInsets.all(15),
                 animationDuration: Duration(milliseconds: 300),
                 borderRadius: 10,
-                onOpen: () => print('cell 1 opened'),
-                onClose: () => print('cell 1 closed')),
-            SimpleFoldingCell(
-                key: _foldingCellKey2,
-
-                frontWidget: _buildFrontWidget(_foldingCellKey2, "CARD 2"),
-                innerTopWidget: _buildInnerTopWidget(_foldingCellKey2,"TITLE 2", context),
-                innerBottomWidget: _buildInnerBottomWidget(_foldingCellKey2,context),
-                cellSize: Size(MediaQuery.of(context).size.width, 125),
-                padding: EdgeInsets.all(15),
-                animationDuration: Duration(milliseconds: 300),
-                borderRadius: 10,
-                onOpen: () => print('cell 2 opened'),
-                onClose: () => print('cell 2 closed')),
-            SimpleFoldingCell(
-                key: _foldingCellKey3,
-                frontWidget: _buildFrontWidget(_foldingCellKey3, "CARD 2"),
-                innerTopWidget: _buildInnerTopWidget(_foldingCellKey3,"TITLE 2", context),
-                innerBottomWidget: _buildInnerBottomWidget(_foldingCellKey3,context),
-                cellSize: Size(MediaQuery.of(context).size.width, 125),
-                padding: EdgeInsets.all(15),
-                animationDuration: Duration(milliseconds: 300),
-                borderRadius: 10,
-                onOpen: () => print('c  ell 2 opened'),
-                onClose: () => print('cell 2 closed')),
-          ],
-        ),
-      ),
+                onOpen: () => print('$index cell opened'),
+                onClose: () => print('$index cell closed'));
+          }),
     );
   }
 
-  Widget _buildFrontWidget(
-      GlobalKey<SimpleFoldingCellState> key, String title) {
-    return Material(
-      child: InkWell(
-        onTap: (){
-          key?.currentState?.toggleFold();
+  Widget _buildFrontWidget(int index) {
+    return Builder(
+      builder: (BuildContext context) {
+        return Material(
+          child: InkWell(
+              onTap: () {
+                SimpleFoldingCellState foldingCellState =
+                context.ancestorStateOfType(
+                    TypeMatcher<SimpleFoldingCellState>());
+                foldingCellState?.toggleFold();
+              },
+            child: Container(
+                color: Color(0xFFffcd3c),
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("${temp[index]}",
+                        style: TextStyle(
+                            color: Color(0xFF2e282a),
+                            fontFamily: 'OpenSans',
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w800)),
+//                    FlatButton(
+//                      onPressed: () {
+//                        SimpleFoldingCellState foldingCellState =
+//                        context.ancestorStateOfType(
+//                            TypeMatcher<SimpleFoldingCellState>());
+//                        foldingCellState?.toggleFold();
+//                      },
+//                      child: Text(
+//                        "Open",
+//                      ),
+//                      textColor: Colors.white,
+//                      color: Colors.indigoAccent,
+//                      splashColor: Colors.white.withOpacity(0.5),
+//                    )
+                  ],
+                )),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildInnerTopWidget(int index) {
+    return Builder(builder: (context)
+    {
+      return FlatButton(
+        padding: EdgeInsets.all(0),
+        onPressed: () {
+          SimpleFoldingCellState foldingCellState = context
+              .ancestorStateOfType(TypeMatcher<SimpleFoldingCellState>());
+          foldingCellState?.toggleFold();
         },
         child: Container(
-            color: Color(0xFFffcd3c),
+            color: Color(0xFFff9234),
             alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(title,
-                    style: TextStyle(
-                        color: Color(0xFF2e282a),
-                        fontFamily: 'OpenSans',
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w800)),
-//            FlatButton(
-//              onPressed: () => key?.currentState?.toggleFold(),
-//              child: Text(
-//                "Open",
-//              ),
-//              textColor: Colors.white,
-//              color: Colors.indigoAccent,
-//              splashColor: Colors.white.withOpacity(0.5),
-//            )
-              ],
-            )),
-      ),
-    );
-  }
-
-  Widget _buildInnerTopWidget(GlobalKey<SimpleFoldingCellState> key,String title, BuildContext context) {
-    return InkWell(
-      onTap: () => key?.currentState?.toggleFold(),
-      child: Container(
-          color: Color(0xFFff9234),
-          alignment: Alignment.center,
-          child: InkWell(
-            onTap: () => key?.currentState?.toggleFold(),
-            child: Text(title,
+            child: Text("${value[index]}",
                 style: TextStyle(
                     color: Color(0xFF2e282a),
                     fontFamily: 'OpenSans',
                     fontSize: 20.0,
-                    fontWeight: FontWeight.w800)),
-          )),
-    );
+                    fontWeight: FontWeight.w800))),
+      );
+    });
   }
 
-  Widget _buildInnerBottomWidget(GlobalKey<SimpleFoldingCellState> key,context) {
-    return InkWell(
-      onTap: () => key?.currentState?.toggleFold(),
-      child: Container(
-        color: Color(0xFFecf2f9),
-        alignment: Alignment.bottomCenter,
-        child: Padding(
-          padding: EdgeInsets.only(bottom: 10),
-        child: FlatButton(
-          onPressed: () => print("sdas"),
-          child: Text(
-            "Close",
+  Widget _buildInnerBottomWidget(int index) {
+    return Builder(builder: (context) {
+      return FlatButton(
+        padding: EdgeInsets.all(0),
+        onPressed: () {
+          SimpleFoldingCellState foldingCellState = context
+              .ancestorStateOfType(TypeMatcher<SimpleFoldingCellState>());
+          foldingCellState?.toggleFold();
+        },
+        child: Container(
+          color: Color(0xFFecf2f9),
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 10),
+//            child: FlatButton(
+//              onPressed: () {
+//                SimpleFoldingCellState foldingCellState = context
+//                    .ancestorStateOfType(TypeMatcher<SimpleFoldingCellState>());
+//                foldingCellState?.toggleFold();
+//              },
+//              child: Text(
+//                "Close",
+//              ),
+//              textColor: Colors.white,
+//              color: Colors.indigoAccent,
+//              splashColor: Colors.white.withOpacity(0.5),
+//            ),
           ),
-//          textColor: Colors.white,
-//          color: Colors.indigoAccent,
-//          splashColor: Colors.white.withOpacity(0.5),
         ),
-        ),
-      ),
-    );
+      );
+    });
   }
 }
